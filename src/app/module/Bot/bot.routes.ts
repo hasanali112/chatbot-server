15d79a6aa1fb5452da5fc4171c717734c9ba5a27
hasanali112 <sohagali.ru.ac@gmail.com>
2025-06-19 22:51:16 +0600

    add

--- src/app/module/Bot/bot.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { BotController } from "./bot.controller";
-
-const router = Router();
-
-router.get("/chat", BotController.createChat);
-
-export const BotRoutes = router;
